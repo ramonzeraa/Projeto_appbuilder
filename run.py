b264b1bad3ede82bcd3ef96c569c09782b34337b
ramonzeraa <ramoncandido64@gmail.com>
2024-10-11 15:40:23 -0300

    Iniciado o run.py com a inicialização do flask, logging, appbuilder e SQLA

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 from app import app
+import logging
+
+from flask import Flask
+from flask_appbuilder import AppBuilder, SQLA
+
+
+logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
+logging.getLogger().setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+app.config.from_object("config")
+db = SQLA(app)
+appbuilder = AppBuilder(app, db.session)
+
+from . import views
+
 
 app.run(host="0.0.0.0", port=8080, debug=True)
