791fa1f64684c7b7cc4f5c7fb6543e09cfa7b5e8
ramonzeraa <ramoncandido64@gmail.com>
2024-10-11 15:42:04 -0300

    Modelo ALUNO definido

--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
-from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
+from flask_appbuilder import Model
 
-"""
+class Aluno(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique = True, nullable=False)
+    matricula = Column(String(20), unique = True, nullable=False)
 
-You can use the extra Flask-AppBuilder fields and Mixin's
-
-AuditMixin will add automatic timestamp of created and modified by who
-
-
-"""
+    def __repr__(self):
+        return self.name
